d6d90e1629edd371b9a3921f5b29cbf6525743ab
SIST <SIST@DESKTOP-0RNH3HN>
2023-04-03 18:24:19 +0900

    1111

---
+++ Chapter2_변수와연산자/src/정리_03_29.java
@@ -0,0 +1,50 @@
+/*
+ * 		4page
+ * 		A.java  =========> A.class  ========> 화면에 결과값 출력
+ * 				컴파일(javac)			인터프리터(java)   ===>동시에처리(ctrl+f11)
+ * 
+ * 		5page : 운영체제에 독립적이다 (운영체제마다 같은 번역을 가지고 있다)
+ * 		
+ * 		19page
+ * 		  1) 파일명과 클래스명이 동일해야 함.
+ * 		  2) 가급적이면 한개의 파일에 하나의 클래스만 사용 권장함. / 만약 클래스가 어러개 있다면 public 옆에 있는 값이 파일 저장명. > main> 암거나
+ * 		  3) 클래스명은 대문자로 시작 권장. 
+ * 		  4) 실행을 위해서는 Mainmethod가 필요함.
+ * 		  
+ * 			자바에 화면 출력 메소드
+ * 			System.out.print()  => 바로 옆에 출력
+ * 			System.out.println()  => \n을 포함, 다음줄에 출력
+ * 			System.out.printf() => 출력 형식을 만들 때
+ * 
+ * 
+ * 		22page : 들여쓰기 {} 쓰면 한번에 써두기
+ * 		// 주석을 이용해서 어떤 코딩을 했는지 미리 써두기
+ * 		{
+ * 
+ * 		}
+ * 
+ * 		23page
+ * 			주석 : 번역이 안되는 파트 //
+ * 		25page
+ * 			변수 : 1개의 데이터만 저장할 수 있음.
+ * 				: 데이터값을 변경할 수 있다.
+ * 		26page
+ * 			변수 선언 규칙
+ * 			데이터형 변수명= 초기값
+ * 			예) int a=10
+ * 
+ * 				A+ 만들어보기
+ * 				char a='A'; char op=:'+';
+ * 				System.out.println(a+""+op);
+ * 
+ * 
+ * 
+ */
+public class 정리_03_29 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
